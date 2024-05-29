7631a0bbeac4de7a5331ec29075876fdc37abb66
최선화 <161446039+sunhew@users.noreply.github.com>
2024-05-29 18:21:44 +0900

    Update dessert39Coffee.py

--- dessert39Coffee.py
+++ dessert39Coffee.py
@@ -37,8 +37,8 @@ tracks = soup.select("#list_98 .product")
 for track in tracks:
     title = track.select_one(".product > p.tit").text.strip()    
     image_url = track.select_one(".product > .frame > img").get('src')
-    SubTitle = track.select_one(".popup-wrap > .popup popup_view > .info.pdinfo > p.engtit").text.strip()
-    content = track.select_one(".popup-wrap > .popup popup_view > .info.pdinfo > p.detail").text.strip()
+    SubTitle = track.select_one(".popup-wrap > .popup.popup_view > .info.pdinfo > p.engtit").text.strip()
+    content = track.select_one(".popup-wrap > .popup.popup_view > .info.pdinfo > p.detail").text.strip()
   
     coffee_data.append({
         "title": title,
